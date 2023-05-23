da27d41496ee25f1268a0f65bfca0136559255db
Dewcat <tokiomi916832801@gmail.com>
2023-05-23 22:01:20 +0800

    新功能：读写json和自然语言正则匹配

---
+++ data.py
@@ -0,0 +1,17 @@
+import json
+def read_data():
+    with open("data.json",'r',encoding='utf-8') as load_f:
+        load_data = json.load(load_f)
+    return load_data
+def write_data(attr,value):
+    load_data=read_data()
+    load_data[attr]=value
+    with open("data.json",'w',encoding='utf-8') as f:
+        json.dump(load_data, f,ensure_ascii=False)
+def get_data(attr):
+    load_data=read_data()
+    return load_data[attr]
+
+if __name__ == '__main__':
+    # print(read_data())
+    write_data("VIS",get_data("VIS")+1)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from poker import draw
 from poker import draw_five
 import event
 from event import dice
+import data
+import re
 # 初始化Bot，国内需要增加proxy代理
 # Updater更新者可以按照给定的TOKEN取得Telegram上的所有事件
 updater = Updater(token=token, use_context=False, request_kwargs={'proxy_url': 'socks5h://127.0.0.1:7890/'})
@@ -18,11 +20,13 @@ dispatch = updater.dispatcher
 
 # 添加指令方法
 def start(bot, update):
-    print(bot)  # 机器人信息，包含机器人ID,用户名，昵称
-    print(update)  # 负责处理本次通讯的请求和响应
+    # print(bot)  # 机器人信息，包含机器人ID,用户名，昵称
+    # print(update)  # 负责处理本次通讯的请求和响应
     message = update.message
     chat = message['chat']
-    update.message.reply_text(text="Hello world")  # 返回普通文本
+    txt=message['text']
+    print(txt)
+    update.message.reply_text(text="hello world")  # 返回普通文本
 def poker(bot, update):
     you=draw()
     message = update.message
@@ -49,11 +53,21 @@ def reset(bot, update):
     chat = message['chat']
     update.message.reply_text(text="事件重置完毕")
 
+def test(context,update):
+    message = update.message
+    txt=message.text
+    s=re.search(r"\d+",txt)
+    # print(s.group())
+    update.message.reply_text(text="已增加"+s.group()+"点攻击力")
+
+
 # 注册指令到调度器
 dispatch.add_handler(CommandHandler('start', start))
 dispatch.add_handler(CommandHandler('poker', poker))
 dispatch.add_handler(CommandHandler('five', five))
 dispatch.add_handler(CommandHandler('roll', roll))
 dispatch.add_handler(CommandHandler('reset', reset))
+filter_test=Filters.regex("增加\d点攻击力")
+dispatch.add_handler(MessageHandler(filter_test,test))
 if __name__ == '__main__':
     updater.start_polling()  # 启动Bot
