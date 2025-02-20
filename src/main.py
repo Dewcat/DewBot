616d65c6b9db639090652a5e87912e4693307b29
DewCat <916832801@qq.com>
2025-02-20 16:12:05 +0800

    添加 .gitignore 文件，忽略编译文件和环境变量，更新主程序以加载环境变量

--- src/main.py
+++ src/main.py
@@ -1,32 +1,27 @@
+import os
+from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler, CallbackContext, ConversationHandler, MessageHandler, filters
 from telegram import Update
-# import logging
-# from game.dice import DiceGame
-# from database.queries import get_skill_info, get_character_stats, update_character_health, reset_character_stats, update_character_strength, update_character_weakness
-# from game.dice_roll import dice_command
 from stat_checks import get_stat_handlers
 from attribute_modifiers import get_attribute_modifier_handlers
 from stat_panel import get_stat_panel_handler
 from battle_defense import get_battle_conv_handler, get_defense_conv_handler
 from character_management import get_character_management_handlers
 
-token = "5769924134:AAFr2m3_mr_HbOdO42Mt8MUGAxFAaG4E4Yg"
+load_dotenv()
+
+token = os.getenv("TELEGRAM_TOKEN")
 
 # 初始化
 application = Application.builder().token(token).proxy('socks5://127.0.0.1:7890').build()
 
-
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('miamiamiamiaminmi')
-
-
 
 application.add_handler(CommandHandler("start", start))
 
 for handler in get_character_management_handlers():
     application.add_handler(handler)
-
-# application.add_handler(MessageHandler(filters.Regex(r'^\d*d\d+([+-]\d+)?$'), dice_command))  # 添加掷骰命令处理程序
 
 application.add_handler(get_battle_conv_handler())
 application.add_handler(get_defense_conv_handler())
